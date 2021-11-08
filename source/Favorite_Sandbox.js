98295dee520a1db8d5a688fa8791467dcf5d72a1
Niya Shao <doreen0niya@users.github.com>
2021-11-07 16:29:43 -0800

    Update Filter, Initiate Favorite

--- source/Favorite_Sandbox.js
+++ source/Favorite_Sandbox.js
@@ -1,12 +1,113 @@
-var currentRecipe = a; // the recipe that the user is currently looning at.
+window.addEventListener('DOMContentLoaded', init);
 
-//get storage
-myStorage = window.localStorage;
-//get list of favorite recipes
-let favoriteRecipes = localStorage.getItem('favorite_recipes'); // This is a string
-function storeIntoFav(obj){  // Translate string to an array? Can we directly store and get an array object?
-    let stored = false;
-    
+//a list of populated recipes for testing
+let recipes = []
+//separtor to separate between recipes for favorite_recipes string stored in local storage.
+let separator = "<SEPARATOR-SAPARATOR>"
+
+//Helper functions to display json recipe object on the screen
+function logTheObj(obj) {
+    var ret = "";
+    ret = obj['id'] + obj['title']
+    return "<ul>" + ret + "</ul>";
 }
 
-//
+//store the newly added recipe into the list
+//structure localStorage.getItem('favorite_recipes') get a string of recipes
+//format: <SEPARATOR-SEPARATOR> + JSON.stringify(recipe1) + <SEPARATOR-SEPARATOR> + JSON.stringify(recipe2) + <SEPARATOR-SEPARATOR> + etc.
+function storeIntoFav(obj){  // Translate string to an array? Can we directly store and get an array object?
+    
+    //console.log(localStorage.getItem('favorite_recipes'));
+
+    //When store the first favorite recipe, localStorage.getItem('favorite_recipes') = null, to prevent becomes null + RecipeContent, remove the null
+    if (localStorage.getItem('favorite_recipes') == null){
+        localStorage.setItem('favorite_recipes', '');
+    }
+
+    //get the current list of favorite recipes
+    let favoriteRecipes = ''
+    try{
+        favoriteRecipes = localStorage.getItem('favorite_recipes'); // This is a string
+    }
+    catch(e){
+        console.log('WARNING!!: favorite_recipes not grabbed')
+    }
+
+    //store the passed in recipe 'obj' into favorite_recipes
+    favoriteRecipes += separator+JSON.stringify(obj);
+    try {
+        localStorage.setItem('favorite_recipes', favoriteRecipes);
+        console.log(favoriteRecipes);
+        stored = true;
+    }
+    catch (e){
+        console.log('WARNING!!: Recipe not stored')
+    }
+}
+
+function Display(){
+    //display current favorite recipes oon the screen
+    let favorite_recipes_arr = localStorage.getItem('favorite_recipes').split(separator);
+    //console.log(favorite_recipes_arr);
+    let jsonContent = "";
+    for (let i = 1; i < favorite_recipes_arr.length; i++){
+        jsonContent = jsonContent + logTheObj(JSON.parse(favorite_recipes_arr[i]));
+    }
+    document.querySelector(".card-body").innerHTML =
+    `${jsonContent}`;
+}
+
+//TODO:
+function DeleteFromFav(obj){  // Translate string to an array? Can we directly store and get an array object?
+    favoriteRecipes += separator+JSON.stringify(obj);
+    try {
+        localStorage.setItem('favorite_recipes', favoriteRecipes);
+        stored = true;
+    }
+    catch (e){
+        console.log('WARNING!!: Recipe not stored')
+    }
+}
+
+//bind favorite button
+function bindButton(){
+    const SearchButton = document.querySelector('button');
+    SearchButton.addEventListener('click', function(event){
+        //Currently only storing the first recipe in the populated recipe list for testing
+        storeIntoFav(recipes[0]['recipes'][0]);
+        Display();
+    });
+}
+
+//populate recipe list randomly N recipes for testing
+async function populateRecipe(N){
+    return new Promise((resolve, reject) => {
+        for(let i = 0; i < N; i++){
+            fetch(`https://api.spoonacular.com/recipes/random?apiKey=67931e62b88649359913dbc496b0ad08`).then((response) => {
+                //console.log(response);
+                return response.json();
+            }).then((data) => {
+                recipes.push(data);
+                if(Object.keys(recipes).length == N){resolve(true);}
+            }).catch((error) => {
+                reject(false);
+            })
+        }
+    })
+}
+
+//initiate the sandbox
+async function init() {
+    //clear so that each run is clear
+    window.localStorage.clear();
+    
+    let fetchSucessful = await populateRecipe(1);
+
+    //console.log(recipes)
+    if(!fetchSucessful){
+        console.log('no recipes populated, an error occurs');
+        return;
+    }
+
+    bindButton();
+}
--- source/Filters_Sandbox.js
+++ source/Filters_Sandbox.js
@@ -2,14 +2,37 @@ window.addEventListener('DOMContentLoaded', init);
 
 const MAX_RECIPE_TIME = 100;
 
+//The recipes list contain json objects of recipes
+let recipes = [];
+
 //create lists of types checked
+//the types of recipes input: breakfast, lunch, main course, snack
 let type = [];
-let timeMin = 0;
+//maximum cooking time
 let timeMax = MAX_RECIPE_TIME;
+//a list of allergies / intolerance
 let allergies = [];
+//the name of diet: there can only be one diet choosen.
 let diet = "";
 
-//Helper functions
+//populate recipe list randomly for testing
+async function populateRecipe(N){
+    return new Promise((resolve, reject) => {
+        for(let i = 0; i < N; i++){
+            fetch(`https://api.spoonacular.com/recipes/random?apiKey=67931e62b88649359913dbc496b0ad08`).then((response) => {
+                //console.log(response);
+                return response.json();
+            }).then((data) => {
+                recipes.push(data);
+                if(Object.keys(recipes).length == N){resolve(true);}
+            }).catch((error) => {
+                reject(false);
+            })
+        }
+    })
+}
+
+//Helper functions to display json recipe object on the screen
 function logTheObj(obj) {
     var ret = "";
     for (var o in obj) {
@@ -21,8 +44,9 @@ function logTheObj(obj) {
         }
     }
     return "<ul>" + ret + "</ul>";
-  }
-  
+}
+
+//Helper function: fetch a random recipe from spoonacular
 function fetchCall(query){
 fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=67931e62b88649359913dbc496b0ad08&${query}&instructionsRequired=true&addRecipeInformation=true`).then((response) => {
     console.log(response);
@@ -36,12 +60,42 @@ fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=67931e62b8864935
 });
 }
 
-//TODO
-function filter(json, type_, timeMin_, timeMax_, allergies_, diet_){
-
+//'manually' filter in local storage instead of a API call
+//Implemented: filter by types[] MaxTime
+//TODO: filter by allergies (intolenrance): This property is not defined
+//in the data json. Thus to implement we have to find the ingredients, then
+//define which terms we are searching for (eg, fish = salmon, albacore, etc)
+function filter(json_arr, type_, timeMax_, allergies_, diet_){
+    let filtered = []
+    bool_filt = true;
+    for(let recipe of json_arr){
+        recipe = recipe['recipes'][0];
+        console.log("keys: " + Object.keys(recipe));
+        let b = false;
+        for(let t in type_){
+            if(t in recipe['dishTypes']){
+                b = true;
+            }
+        }
+        if (b === false){continue;}
+        if (timeMax_ < recipe['readyInMinutes']){continue;}
+        // for(let a in allergies_){
+        //     if(a in recipe['dishTypes']){
+        //         b = true;
+        //     }
+        // }
+        // let b = false;
+        // for(let d in diet_){
+        //     if(d in recipe['dishTypes']){
+        //         b = true;
+        //     }
+        // }
+        // if (b === false){continue;}
+        filtered.push(recipe);
+    }
 }
 
-function init() {
+function bindButton(){
     const SearchButton = document.querySelector('button');
     SearchButton.addEventListener('click', function(event){
         //Add the checked checkbox into the list
@@ -78,8 +132,28 @@ function init() {
         //Diet need to be make sure that only one checkbox is checked at a time
         const cb_recipe_diets = document.getElementById('recipe_diets');
         if(cb_recipe_diets.checked){
-            diet = "vegan";
+            diet = "ketogenic";
         }
+//matching with API: diets
+//in design channel graph - input to the API
+//keto - ketogenic
+//paleo - paleo
+//vegetarian - vegetarain
+//mediterranean - not exist
+//raw - not exsit
+//low carb - gluten free
+//no sugar - not exist
+
+//matching with API: allergies / intolerance
+//in design channel graph - input to the API
+//fish - seafood
+//dairy - dairy
+//tree nut - tree nut
+//shellfish - shellfish
+//eggs - egg
+//peanut - peanut
+//soy - soy
+//wheat - wheat
 
         //Filter recipes according to list content
         //if use spoonacular API:
@@ -87,8 +161,19 @@ function init() {
         "type=" + type.join(',') + "&" +
         "maxReadyTime=" + timeMax + "&" +
             "diet=" + diet);
-        //TODO: only one diet, no minReadyTime
-        //fetchCall("diet=vegan");
 
+        //if use local storage filter function created ourselves
+        filter(recipes, type, timeMin, timeMax, allergies, diet);
     });
+}
+
+//initiate the sandbox
+async function init() {
+    let fetchSucessful = await populateRecipe(5);
+    console.log(recipes)
+    if(!fetchSucessful){
+        console.log('no recipes populated, an error occurs');
+        return;
+    }
+    bindButton()
 }
