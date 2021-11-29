a5c57046b47d1e1b42f79f992349659e2db19f31
Richard <Richard511011@gmail.com>
2021-11-28 22:22:10 -0800

    Basic create feature done,TODO: error handling for invalid URL's, styling for this page

--- source/pages/MyRecipesPage/myRecipePage.js
+++ source/pages/MyRecipesPage/myRecipePage.js
@@ -206,166 +206,6 @@ function greyOut (on) {
  * Alters the recipe cards on the main page to display the recipes retrieved by search
  * @param {Object} recipeData the object containing JSON recipe data
  */
-<<<<<<< HEAD
- function createRecipeCards (recipeData) {
-    // clear loaded recipe cards
-    const recipesContainer = document.querySelector('.recipes-container')
-    while (recipesContainer.firstChild) {
-        recipesContainer.removeChild(recipesContainer.firstChild)
-    }
-    for (let i = 0; i < recipeData.length; i++) {
-        // delegates the creation of recipe-card and its content to RecipeCard.js
-        const recipeCard = document.createElement('recipe-card')
-        recipeCard.data = recipeData[i]
-        recipeCard.id = recipeData[i].id
-
-        console.log(recipeData[i].title)
-
-        document.querySelector('.recipes-container').appendChild(recipeCard)
-
-        recipeCard.setAttribute('name', recipeData[i].title)
-        recipeCard.setAttribute('image', recipeData[i].image)
-
-        recipeCard.shadowRoot.querySelector('span').innerText = recipeData[i].title
-        recipeCard.shadowRoot.querySelector('div').style.backgroundImage = `url(${recipeData[i].image})`
-        console.log(recipeCard.id)
-    }
-    
-} 
-/**
- * Adds new recipe to My Recipes page from user imput
- * @param {string} URL the link to the recipe we need to extract
- */ 
-    
-function addRecipeURL(URL) {
-    //make api call to spoonacular
-    fetch(`https://api.spoonacular.com/recipes/extract?apiKey=99a52ef738514021ab33c7e15116c1ca&url=${URL}`)
-    .then((response) => {
-        //converting file to json format
-        return response.json()
-    }).then((recipeData) => {
-        //insert data into recipe array and repopulate local storage
-        const localStorage = window.localStorage
-        let inList = false
-        recipeData.id = hashing(recipeData.title)
-        console.log(recipeData.id)
-        //if there isnt a duplicate recipe, add it to myRecipe;
-        if (localStorage.getItem(recipeData.id) != null) {
-            console.log("DUPLICATE RECIPE")
-        } else {
-            localStorage.setItem(recipeData.id, JSON.stringify(recipeData))
-            //repopulate myrecipepage
-            populateMyRecipe();
-            resolve(true);
-        }
-        // if(Object.keys(recipes).length == N){resolve(true);}
-    }).catch((error) => {
-        reject(false);
-        alert("invalid URl");
-    })
-}
-function hashing(string) {
-    //set variable hash as 0
-    var hash = 0;
-    // if the length of the string is 0, return 0
-    if (string.length == 0) return hash;
-    for (i = 0; i < string.length; i++) {
-        ch = string.charCodeAt(i);
-        hash = ((hash << 5) - hash) + ch;
-        hash = hash & hash;
-    }
-    return hash;
-
-}
-
-//logic for clicking the button
-function addButtonOnClick(){
-
-
-}
-//initiate the sandbox
-async function init() {
-    //clear so that each run is clear
-    // window.localStorage.clear();
-
-    // let fetchSucessful = await populateRecipe(5);
-
-    // //console.log(recipes)
-    // if(!fetchSucessful){
-    //     console.log('no recipes populated, an error occurs');
-    //     return;
-    // }
-
-    // console.log(recipes)
-    // console.log(recipes[0]['recipes'][0])
-
-    // for (let recipe of recipes) {
-    //     storeIntoFav(recipe['recipes'][0]);
-    // }
-
-    populateMyRecipe()
-
-
-    console.log("GREY TEST" +document.querySelector('recipe-card'))
-
-    let  gflag = true;
-    
-    // document.querySelector('recipe-card').shadowRoot.querySelector('.recipe-card').classList.add("grey") 
-    // document.querySelector('recipe-card').shadowRoot.querySelector('.recipe-card').classList.remove("grey")
-    document.getElementById("select-button").addEventListener("click", () => {
-
-        //grey all of the recipe-card
-        greyOut(true);
-        //set selected to true;
-        selected = true
-
-    })
-
-    // logic for cancel button
-    document.getElementById("cancel-button").addEventListener("click", () => {
-        
-        // remove all the grey class
-        greyOut(false);  
-        // set selected to false
-        selected = false;
-    })
-
-
-
-    
-    
-    //     //Code for adding myRecipePage event listener to each recipe card
-    // let recipeCardList = Array.from(document.querySelectorAll('recipe-card'));
-    // for(let i=0;i<recipeCardList.length;i++){
-    //     recipeCardList[i].addEventListener("click",(e)=>{
-    //     //console.log(recipeCardList[i])
-    //     if(selected){
-    //         //if the grey class exist
-    //         // console.log(e)
-    //     if (recipeCardList[i].shadowRoot.querySelector('.recipe-card').classList.contains('grey')){
-    //     recipeCardList[i].shadowRoot.querySelector('.recipe-card').classList.remove('grey')
-        
-    //     }else{
-    //     recipeCardList[i].shadowRoot.querySelector('.recipe-card').classList.add('grey')    
-    //     }
-    //     }
-    //     })        
-    //}
-
-
-    
-    // console.log("TEST DELETE FOR ID: " + document.querySelector('recipe-card').id + " with title " + document.querySelector('recipe-card').getAttribute("name"))
-    // document.getElementById("remove-button").addEventListener("click", ()=>{
-  
-    //     console.log('clicked')
-    //     console.log(document.querySelector('recipe-card').id)
-    //     DeleteFromFavID(document.querySelector('recipe-card').id)
-    // })
-    addRecipeURL("https://natashaskitchen.com/pan-seared-steak/")
-    document.querySelector('#add-button').addEventListener('click', ()=>{addButtonOnClick()})
-    bindRemoveButton()
-}
-=======
 function createRecipeCards (recipeData) {
   // clear loaded recipe cards
   const recipesContainer = document.querySelector('.recipes-container')
@@ -414,6 +254,70 @@ function createRecipeCards (recipeData) {
 //   }
 // }
 
+// Code for adding recipe by URL Buttons
+ function addButtonOnClick(){
+    //  alert('clicked')
+    document.querySelector('.addPage').classList.remove('hide');
+    document.querySelector('.recipes-container').classList.add('hide');
+}
+
+   function addRecipeURL(URL) {
+        console.log(`https://api.spoonacular.com/recipes/extract?apiKey=99a52ef738514021ab33c7e15116c1ca&url=${URL}`)
+    //make api call to spoonacular
+    fetch(`https://api.spoonacular.com/recipes/extract?apiKey=aebc3ef46cd54888b77ec872fa50deb1&url=${URL}`)
+    .then((response) => {
+        //converting file to json format
+        return response.json()
+    }).then((recipeData) => {
+        //insert data into recipe array and repopulate local storage
+        const localStorage = window.localStorage
+        let inList = false
+        recipeData.id = hashing(recipeData.title)
+        console.log(recipeData.id)
+        //if there isnt a duplicate recipe, add it to myRecipe;
+        if (localStorage.getItem(recipeData.id) != null) {
+            console.log("DUPLICATE RECIPE")
+        } else {
+            localStorage.setItem(recipeData.id, JSON.stringify(recipeData))
+            //repopulate myrecipepage
+            populateMyRecipe();
+            resolve(true);
+        }
+        // if(Object.keys(recipes).length == N){resolve(true);}
+    })
+    // .catch((error) => {
+    //     reject(false);
+    //     alert("invalid URl");
+    // })
+     document.querySelector('.addPage').classList.add('hide');
+    document.querySelector('.recipes-container').classList.remove('hide');
+}
+function hashing(string) {
+    //set variable hash as 0
+    var hash = 0;
+    // if the length of the string is 0, return 0
+    if (string.length == 0) return hash;
+    for (i = 0; i < string.length; i++) {
+        ch = string.charCodeAt(i);
+        hash = ((hash << 5) - hash) + ch;
+        hash = hash & hash;
+    }
+    return hash;
+
+}
+function addPageSubmitOnClick(){
+    let URL = document.querySelector('.addPageText').value;
+    console.log(URL);
+
+    addRecipeURL(URL)
+     document.querySelector('.addPageText').value ="";
+
+}
+function addPageCloseOnClick(){
+    document.querySelector('.addPageText').value ="";
+    document.querySelector('.addPage').classList.add('hide');
+    document.querySelector('.recipes-container').classList.remove('hide');
+}
 // initiate the sandbox
 async function init () {
   // clear so that each run is clear
@@ -459,6 +363,11 @@ async function init () {
     selected = false
     bindRecipeCards()
   })
+  //Code for adding recipe by URL:
+  document.querySelector('.add-button').addEventListener('click', ()=>{addButtonOnClick()})
+
+    document.querySelector('.addPageSubmit').addEventListener('click', ()=>{addPageSubmitOnClick()})
+    document.querySelector('.addPageClose').addEventListener('click', ()=>{addPageCloseOnClick()})
 
   //     //Code for adding myRecipePage event listener to each recipe card
   // let recipeCardList = Array.from(document.querySelectorAll('recipe-card'));
@@ -488,4 +397,3 @@ async function init () {
 
   bindRemoveButton()
 }
->>>>>>> 9b13002e31dd05819e291c850491efbf55a8d48a
